c98508621792cd41d669e788f07ee2f0ea71ecd9
MiguelMartins12345 <n3921571@gmail.com>
2025-02-12 14:49:59 +0000

    update

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -6,7 +6,12 @@ export default function ComponentTesting() {
         <h1 className="text-3xl font-bold text-center mb-8">Component Testing Page</h1>
         
         <section>
-          <DoctorProfileCard />
+          <div>
+            <DoctorProfileCard />
+          </div>
+          <div>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDH6ObZa9dSX4cn9O8wD2OaVUjR0ABj-RA&libraries=places"></script>
+          </div>
         </section>
 
       </div>
